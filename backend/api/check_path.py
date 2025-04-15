f637117642dbc1d3fdbc7c4a8edabd5991c34965
KALUSANI SANTHOSH REDDY <chintusanthosh2004@gmail.com>
2025-04-15 22:29:21 +0530

    Delete backend directory

--- backend/api/check_path.py
+++
@@ -1,9 +0,0 @@
-import os
-
-dataset_path = r"C:\Users\chint\Documents\Facial_Recognition_project\dataset"
-
-if os.path.exists(dataset_path):
-    print("✅ Folder found! Listing files:")
-    print(os.listdir(dataset_path))
-else:
-    print("❌ Error: 'dataset' folder not found!")
